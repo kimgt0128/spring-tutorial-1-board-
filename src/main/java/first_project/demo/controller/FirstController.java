051fb9a0b1a326e6a4b1682c729dd56cd56ca1f7
KT <kimgt2828@gmail.com>
2024-06-01 01:28:05 +0900

    build: Project 생성하고 hello world 띄우기
    Controller 패키지 만들기
    Model을 이용하여 변수 전달
    여러 Templet 페이지 만들고 controller 적용시키기

---
+++ src/main/java/first_project/demo/controller/FirstController.java
@@ -0,0 +1,19 @@
+package first_project.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "KT");
+        return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
+    }
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "홍길동");
+        return "goodbye";
+    }
+}
---
+++ src/test/java/first_project/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package first_project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
